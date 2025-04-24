3352459485850fb222f726cd3ebb68f78823b0c8
vinu033 <vinaykalasannavar124@gmail.com>
2025-04-23 22:43:39 -0700

    save file

---
+++ Arithmatic.java
@@ -0,0 +1,13 @@
+public class Arithmatic{
+
+    public static void main(String[] args) {
+        int num1 = 20;
+        int num2 = 5;
+
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+        int difference = num1 - num2;
+        System.out.println("The difference between " + num1 + " and " + num2 + " is: " + difference);
+    }
+}
