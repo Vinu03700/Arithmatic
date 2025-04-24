24da81b25c70eb6e9c5b4dc811811d972bd6ded8
vinu033 <vinaykalasannavar124@gmail.com>
2025-04-23 22:52:21 -0700

    Update Arithmatic.java

--- Arithmatic.java
+++ Arithmatic.java
@@ -4,10 +4,16 @@ public class Arithmatic{
         int num1 = 20;
         int num2 = 5;
 
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+        int difference = num1 - num2;
+        System.out.println("The difference between " + num1 + " and " + num2 + " is: " + difference);
+
         int mul = num1 * num2;
-        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + mul);
+        System.out.println("The multiflication of " + num1 + " and " + num2 + " is: " + mul);
 
         int div = num1 / num2;
-        System.out.println("The difference between " + num1 + " and " + num2 + " is: " + div);
+        System.out.println("The division " + num1 + " and " + num2 + " is: " + div);
     }
 }
